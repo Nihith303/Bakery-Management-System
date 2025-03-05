da6a883e4da8a939dc1cc6b2c9dcf24c1b9b00b4
Jonnalagadda Nihit <133048404+Nihith303@users.noreply.github.com>
2025-03-05 22:59:33 +0530

    Delete autotyper.py

--- autotyper.py
+++
@@ -1,7 +0,0 @@
-import pyautogui as py
-import time 
-a = input("Enter the Sentence :")
-time.sleep (10) 
-for _ in range(int(input("Enter how many times"))):
-    py.write(a) 
-    py.press("enter")
